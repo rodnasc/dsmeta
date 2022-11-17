649e1bd77dde671f43806aeeb42a302e5b7a075c
Rodrigo Nascimento <rod.nasc@outlook.com>
2022-11-17 14:15:04 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from "../../assets/Img/notification-icon.svg"
+import "./style.css"
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </Div>
+    )
+}
+
+export default NotificationButton
